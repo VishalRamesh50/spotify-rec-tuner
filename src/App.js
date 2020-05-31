ef14b2b8547ccaa647f4f4e3b96d058519461c30
Vishal Ramesh <vishalramesh50@gmail.com>
2020-05-31 16:57:42 -0400

    Create initial home page layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+import SpotifyLoginButton from './components/SpotifyLoginButton/SpotifyLoginButton'
+import './App.css'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <div class="header__bg"></div>
+        <h1>Spotify Recommendation Tuner</h1>
       </header>
+
+      <section className="sign-in-area">
+        <SpotifyLoginButton />
+      </section>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
