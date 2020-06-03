08f5a3ecbfe123f1109e5c32d5da9cbe64ae8c4d
Vishal Ramesh <vishalramesh50@gmail.com>
2020-06-02 23:02:01 -0400

    Adjust middleware to allow origins from a list of hosts

--- src/server/server.js
+++ src/server/server.js
@@ -8,6 +8,16 @@ const refresh = require('./routes/refresh')
 const app = express()
 const port = 3001
 
+const allowOrigin = (req, res, next) => {
+  const origin = req.get('origin')
+  const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001']
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+  }
+  next()
+}
+app.use(allowOrigin)
+
 app.use(login)
 app.use(callback)
 app.use(refresh)
