3d3d23fed117674bd302f607861061d3ff3d8996
Vishal Ramesh <vishalramesh50@gmail.com>
2020-06-05 01:52:45 -0400

    Move from using css to makeStyles
    - Also sliders now have different ranges based on what was passed in for min max values since each parameter actually has different ranges.

--- src/scenes/Tuner/components/Slider/slider.js
+++ src/scenes/Tuner/components/Slider/slider.js
@@ -1,44 +1,92 @@
+import Checkbox from '@material-ui/core/Checkbox'
 import React from 'react'
-import { StylesProvider } from '@material-ui/core/styles'
-import Typography from '@material-ui/core/Typography'
 import Slider from '@material-ui/core/Slider'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
 
-import './slider.css'
+const useStyles = makeStyles(theme => ({
+  selector: {
+    display: 'flex',
+  },
+  slider: {
+    width: '200px',
+    color: '#1db954',
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(0),
+    marginBottom: theme.spacing(1),
+  },
+  sliderName: {
+    margin: theme.spacing(0),
+  },
+  checkbox: {
+    padding: '0 !important',
+    paddingTop: '9px !important',
+    marginRight: '9px',
+  },
+}))
 
-export default function RangeSlider({ name }) {
+const RangeSlider = ({ name, min, max, step }) => {
+  const generateRandomNumber = (min, max, numDecimals) => {
+    return +(Math.random() * (max - min) + min).toFixed(numDecimals)
+  }
   const generateRandomRange = () => {
-    let x = +Math.random().toFixed(2)
-    let y = +Math.random().toFixed(2)
-    while (Math.abs(x - y) < 0.3 || Math.abs(x - y) > 0.7) {
-      x = +Math.random().toFixed(2)
-      y = +Math.random().toFixed(2)
+    const difference = max - min
+    const splitStep = step.toString().split('.')
+    const numDecimals = splitStep.length === 1 ? 0 : splitStep[1].length
+    let x = generateRandomNumber(min, max, numDecimals)
+    let y = generateRandomNumber(min, max, numDecimals)
+    while (
+      Math.abs(x - y) < 0.3 * difference ||
+      Math.abs(x - y) > 0.7 * difference
+    ) {
+      x = generateRandomNumber(min, max, numDecimals)
+      y = generateRandomNumber(min, max, numDecimals)
     }
     return [x, y]
   }
   const [value, setValue] = React.useState(generateRandomRange)
+  const [selected, setSelected] = React.useState(false)
 
-  const handleChange = (event, newValue) => {
+  const classes = useStyles()
+
+  const onSliderChange = (event, newValue) => {
     setValue(newValue)
   }
 
   return (
-    <StylesProvider injectFirst>
-      <Typography id="range-slider" gutterBottom>
-        {name}
-      </Typography>
-      <Slider
-        value={value}
-        className="slider"
-        onChange={handleChange}
-        valueLabelDisplay="auto"
-        aria-labelledby="range-slider"
-        getAriaValueText={() => {
-          return value
+    <div className={classes.selector}>
+      <Checkbox
+        className={classes.checkbox}
+        style={{
+          color: '#1db954',
         }}
-        min={0}
-        max={1}
-        step={0.01}
+        onChange={() => setSelected(!selected)}
       />
-    </StylesProvider>
+      <div>
+        <Typography
+          className={classes.sliderName}
+          id="range-slider"
+          gutterBottom
+        >
+          {name}
+        </Typography>
+        <Slider
+          className={classes.slider}
+          value={value}
+          disabled={!selected}
+          onChange={onSliderChange}
+          valueLabelDisplay="auto"
+          aria-labelledby="range-slider"
+          getAriaValueText={() => {
+            return value
+          }}
+          min={min}
+          max={max}
+          step={step}
+        />
+      </div>
+    </div>
   )
 }
+
+export default RangeSlider
