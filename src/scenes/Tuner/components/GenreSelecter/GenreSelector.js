065fc7f2dde4963221b05507e6aac285c0bb2963
Vishal Ramesh <vishalramesh50@gmail.com>
2020-06-05 01:52:59 -0400

    Created a genre selector for seed genres

---
+++ src/scenes/Tuner/components/GenreSelecter/GenreSelector.js
@@ -0,0 +1,69 @@
+import Autocomplete from '@material-ui/lab/Autocomplete'
+import Checkbox from '@material-ui/core/Checkbox'
+import React from 'react'
+import TextField from '@material-ui/core/TextField'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+  autocomplete: {
+    color: 'white',
+    marginBottom: '20px',
+    width: '200px',
+    '& .MuiOutlinedInput-notchedOutline': {
+      borderColor: '#1db954',
+    },
+  },
+  autocompleteOption: {
+    padding: theme.spacing(0),
+    height: '40px',
+  },
+  input: {
+    color: 'white',
+  },
+}))
+
+export default function CheckboxesTags({ setSelectedSeedGenres }) {
+  const classes = useStyles()
+  const onChange = (event, selectedItems) => {
+    setSelectedSeedGenres(selectedItems)
+  }
+
+  return (
+    <Autocomplete
+      multiple
+      id="checkboxes-tags-demo"
+      className={classes.autocomplete}
+      classes={{
+        option: classes.autocompleteOption,
+      }}
+      options={genres}
+      disableCloseOnSelect
+      onChange={onChange}
+      getOptionLabel={option => option}
+      renderOption={(option, { selected }) => (
+        <React.Fragment>
+          <Checkbox checked={selected} />
+          {option}
+        </React.Fragment>
+      )}
+      renderInput={params => {
+        console.log(params)
+        return (
+          <TextField
+            InputProps={{
+              className: classes.input,
+            }}
+            {...params}
+            variant="outlined"
+            label="Seed Genres"
+            InputLabelProps={{
+              className: classes.input,
+            }}
+          />
+        )
+      }}
+    />
+  )
+}
+
+const genres = ['anime', 'edm', 'jazz', 'k-pop', 'piano', 'pop', 'work-out']
