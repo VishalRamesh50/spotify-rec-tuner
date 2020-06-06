887e365a6d6d78361896faf64f754e634ba0aefd
Vishal Ramesh <vishalramesh50@gmail.com>
2020-06-06 00:51:08 -0400

    Significantly cut down recommendation route by moving refresh logic to a middleware

--- src/server/routes/recommendations.js
+++ src/server/routes/recommendations.js
@@ -1,9 +1,17 @@
 const axios = require('axios')
 const express = require('express')
 
-const router = express.Router()
+const { updateToken } = require('../middleware')
 
-const getRecommendations = async (access_token, params) => {
+const router = express.Router()
+router.use(updateToken)
+
+router.get('/recommendations', async (req, res) => {
+  const access_token = res.locals.access_token
+  const params = { ...req.query }
+  delete params['access_token']
+  delete params['refresh_token']
+
   return axios
     .get('https://api.spotify.com/v1/recommendations', {
       params: params,
@@ -11,50 +19,12 @@ const getRecommendations = async (access_token, params) => {
         Authorization: `Bearer ${access_token}`,
       },
     })
-    .then(res => {
-      console.log('Got a successful response')
-      return res
+    .then(response => {
+      res.send(response.data)
     })
     .catch(err => {
-      console.log(err)
-      return err.response
+      res.status(err.response.status).send()
     })
-}
-
-router.get('/recommendations', async (req, res) => {
-  console.log('Recommendation hit!')
-  let access_token = req.query.access_token
-  const params = { ...req.query }
-  delete params['access_token']
-  delete params['refresh_token']
-
-  let recommendations = await getRecommendations(access_token, params)
-  if (recommendations.status === 401) {
-    console.log('inside if condition')
-    const refresh_token = req.query.refresh_token
-    axios
-      .get(`http://${process.env.EXPRESS_HOST}:3001/refresh`, {
-        params: { refresh_token: refresh_token },
-      })
-      .then(async response => {
-        access_token = response.data.access_token
-        const refresh_token = response.data.refresh_token
-        recommendations = await getRecommendations(access_token, params)
-        if (recommendations.status === 200) {
-          res.cookie('ACCESS_TOKEN', access_token)
-          res.cookie('REFRESH_TOKEN', refresh_token)
-          res.send(recommendations.data)
-        } else {
-          console.log('Tell them to authenticate again')
-          res.redirect(`http://${process.env.EXPRESS_HOST}:3001/login`)
-        }
-      })
-  } else if (recommendations.status === 200) {
-    res.send(recommendations.data)
-  } else {
-    console.error('Something broke badly')
-    res.status(recommendations.status).send()
-  }
 })
 
 module.exports = router
