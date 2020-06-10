459ba77869e53a3f716b9dbec2e3b1fde5c47189
Vishal Ramesh <vishalramesh50@gmail.com>
2020-06-09 20:09:32 -0400

    Renamed ErrorAlert to Alert and generify
    - This alert can be used for either FormErrors or InteractionErrors.
    - The severity and text is adjustable
    - Given a set of errors and a setter it can get rid of the appopriate errors from the list using the given id as the unique indentifier.
    - One thing to note is that `unstable_createMuiStrictModeTheme` is being used instead of createMuiTheme.
    - Without it there will be a bunch of deprecation warnings about findDOMNode. This is a result of MaterialUI not being compliant with the latest React guidlines and is not something fixable with application code other than this method.
    - It's advised not to use it in production applications but I'm not too worried about it in this small site.

---
+++ src/scenes/Tuner/components/Alert.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react'
+import {
+  unstable_createMuiStrictModeTheme as createMuiTheme,
+  makeStyles,
+  ThemeProvider,
+} from '@material-ui/core/styles'
+import Alert from '@material-ui/lab/Alert'
+import IconButton from '@material-ui/core/IconButton'
+import Collapse from '@material-ui/core/Collapse'
+import CloseIcon from '@material-ui/icons/Close'
+
+const darkTheme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+})
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    marginBottom: theme.spacing(2),
+  },
+}))
+
+const TransitionAlerts = ({ id, text, severity, errors, setErrors }) => {
+  useEffect(() => {
+    setOpen(true)
+  }, [errors])
+
+  const [open, setOpen] = useState(true)
+
+  const onExited = () => {
+    let errorsCopy = [...errors]
+    errorsCopy = errorsCopy.filter(err => err.id !== id)
+    setErrors(errorsCopy)
+  }
+
+  const classes = useStyles()
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Collapse in={open} onExited={onExited}>
+        <Alert
+          className={classes.root}
+          severity={severity}
+          action={
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              size="small"
+              onClick={() => setOpen(false)}
+            >
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          }
+        >
+          {text}
+        </Alert>
+      </Collapse>
+    </ThemeProvider>
+  )
+}
+
+export default TransitionAlerts
