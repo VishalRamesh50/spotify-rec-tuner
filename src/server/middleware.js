91ed3bb4ed7e5f407c26f3e9eea13056e9431df8
Vishal Ramesh <vishalramesh50@gmail.com>
2020-06-09 23:29:39 -0400

    Configure the express server to allow these cookies to come through

--- src/server/middleware.js
+++ src/server/middleware.js
@@ -10,6 +10,7 @@ const crossOrigin = (req, res, next) => {
       'Origin, X-Requested-With, Content-Type, Accept',
     )
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    res.header('Access-Control-Allow-Credentials', 'true')
   }
   next()
 }
