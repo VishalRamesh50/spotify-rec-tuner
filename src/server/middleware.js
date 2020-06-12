227a01e3813f65bf86adc0411aa1f17333eba35b
Vishal Ramesh <vishalramesh50@gmail.com>
2020-06-12 00:16:31 -0400

    Create an axios interceptor to handle refreshing the token on 401s
    - Also creates a middlware which is meant to run after all routes to set the access token if has been refreshed

--- src/server/middleware.js
+++ src/server/middleware.js
@@ -1,33 +1,48 @@
 const axios = require('axios')
 
-const updateToken = (req, res, next) => {
-  const refreshToken = req.cookies.REFRESH_TOKEN
+axios.interceptors.response.use(null, error => {
+  console.log('in error interceptor')
+  if (error.response && error.response.status === 401) {
+    const refreshToken = error.config.headers.Cookie.split('=')[1]
 
-  const authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    method: 'POST',
-    headers: {
-      Authorization: `Basic ${Buffer.from(
-        `${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`,
-      ).toString('base64')}`,
-    },
-    params: {
-      refresh_token: refreshToken,
-      grant_type: 'refresh_token',
-    },
-    json: true,
+    const authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      method: 'POST',
+      headers: {
+        Authorization: `Basic ${Buffer.from(
+          `${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`,
+        ).toString('base64')}`,
+      },
+      params: {
+        refresh_token: refreshToken,
+        grant_type: 'refresh_token',
+      },
+      json: true,
+    }
+
+    return axios(authOptions)
+      .then(response => {
+        const accessToken = response.data.access_token
+        error.config.headers.Authorization = `Bearer ${accessToken}`
+        return axios.request(error.config)
+      })
+      .catch(() => {
+        return Promise.reject(error)
+      })
   }
 
-  return axios(authOptions)
-    .then(response => {
-      const { access_token } = response.data
-      res.locals.access_token = access_token
-      res.cookie('ACCESS_TOKEN', access_token, { overwrite: true })
-      next()
-    })
-    .catch(err => {
-      res.status(401).send()
-    })
+  return Promise.reject(error)
+})
+
+const setAccessToken = (req, res, next) => {
+  const { response } = res.locals
+  let access_token = response.request
+    .getHeader('Authorization')
+    .replace('Bearer ', '')
+  if (req.cookies.ACCESS_TOKEN !== access_token) {
+    res.cookie('ACCESS_TOKEN', access_token, { overwrite: true })
+  }
+  res.send(response.data)
 }
 
-module.exports = { updateToken }
+module.exports = setAccessToken
